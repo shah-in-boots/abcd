c7b3e5e95fb0aa6d190daec798437f683201843f
Anish Shah <asshah4@users.noreply.github.com>
2023-05-23 06:40:53 -0500

    updated meta targets

--- R/ecg.R
+++ R/ecg.R
@@ -3,35 +3,27 @@ find_ecg_files <- function(file) {
 }
 
 # May need to batch this due to overall number being about 500k
-convert_ecg_to_cvs <- function(files) {
+convert_ecg_to_wfdb <- function(files) {
 
-	# Original OneDrive folder that files are contained in
-	# Assumes all files have same directory
 	folder <- fs::path_common(files)
 
-	# Individual file names
-
-	# Python script to convert to ECG
-	pyScript <- fs::path(here::here(), "R", "muse_ecg_xml_to_csv.py")
-
-	# Will create a CSV file locally
-	# This needs to move back to the original directory in OneDrive
+	# Read in XML file
 	lapply(files, function(.x) {
 
-		fp <- paste0("'", .x, "'")
-		system2(command = "python",
-						args = c(pyScript, fp),
-						stdout = FALSE)
+		nm <-
+			fs::path_file(.x) |>
+			fs::path_ext_remove()
+
+		ecg <- shiva::read_muse(.x)
+
+		shiva::write_wfdb(
+			data = ecg,
+			type = "muse",
+			record = nm,
+			record_dir = folder,
+			wfdb_path = "/usr/local/bin",
+			header = attr(ecg, "header")
+		)
 
 	})
-
-	# Now all the files are CSV, and will have different names
-	csv_files <-
-		fs::path_file(files) |>
-		fs::path_ext_remove() |>
-		fs::path_ext_set("csv")
-
-	# Move them into the OneDrive folder
-	fs::file_move(csv_files, new_path = folder)
-
 }
--- _targets.R
+++ _targets.R
@@ -6,8 +6,12 @@ tar_option_set(
   packages = c(
     # Systems and cluster computing
     "reticulate", "Microsoft365R",
+    # Personal libraries,
+    "shiva",
+    # Specific data type handling
+    "data.table", "xml2",
     # Wrangling
-    "tibble", "tidyverse"
+    "tibble", "tidyverse", "here", "fs"
     ), # packages that your targets need to run
   format = "rds" # default storage format
   # Set other options as needed.
@@ -28,6 +32,6 @@ list(
   tar_target(data_loc, find_data_folder()),
 
   # ECG pipeline
-  tar_files(ecg_xml_files, find_ecg_files(file.path(data_loc, "signals/muse"))),
-  tar_target(ecg_to_csv, convert_ecg_to_csv(ecg_xml_files))
+  tar_files(ecg_xml_files, find_ecg_files(file.path(data_loc, "cbcd/muse"))),
+  tar_target(ecg_wfdb_format, convert_ecg_to_wfdb(ecg_xml_files))
 )
