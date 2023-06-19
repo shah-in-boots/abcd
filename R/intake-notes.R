c570365ed53dafbe27824c24eb2d0fe84adcd5d5
Anish Shah <asshah4@users.noreply.github.com>
2023-06-19 11:22:08 -0500

    trivial

--- R/intake-notes.R
+++ R/intake-notes.R
@@ -8,6 +8,6 @@ data_dir <- fs::fs_path("/shared/home/ashah282/data/ccts/")
 raw_data <- fs::path(data_dir, 'raw', 'notes.csv')
 
 # Read in data (subset)
-df <- read_csv(raw_data, n_max = 100)
+df <- read_csv(raw_data, n_row = 100)
 
 write_csv(df, fs::path(data_dir, "proc", "notes.csv"))
