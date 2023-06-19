5f054495098e2bd2d88ba7b3d8b479af84e3fbcc
Anish Shah <asshah4@users.noreply.github.com>
2023-06-19 10:23:37 -0500

    shell file for checking directory

--- test-slurm.sh
+++ test-slurm.sh
@@ -7,5 +7,9 @@
 #SBATCH --cpus-per-task=1 		# Number of cores per task
 
 printf 'Loading modules\n'
-module load R/4.1.2-foss-2021b Anaconda3/2022.05
+#module load R/4.1.2-foss-2021b Anaconda3/2022.05
 module list
+
+printf 'Make sure in home directory to access data'
+cd /shared/home/ashah282
+echo $PWD
