4290a2828180f11886ec37dee010c07096acfab8
Anish Shah <asshah4@users.noreply.github.com>
2023-06-19 16:21:33 +0000

    error in datatable

--- R/intake-notes.R
+++ R/intake-notes.R
@@ -2,13 +2,12 @@
 
 library(fs)
 library(tidyverse)
-library(data.table)
 
 # File path
 data_dir <- fs::fs_path("/shared/home/ashah282/data/ccts/")
 raw_data <- fs::path(data_dir, 'raw', 'notes.csv')
 
 # Read in data (subset)
-dt <- fread(raw_data, nrows = 500)
+df <- read_csv(raw_data, n_max = 100)
 
-fwrite(dt, fs::path(data_dir, "proc", "notes.csv"))
+write_csv(df, fs::path(data_dir, "proc", "notes.csv"))
