f80b1ccfb49c707674253a58060c661b8c5a11f5
Anish Shah <asshah4@users.noreply.github.com>
2023-06-19 20:15:30 +0000

    delete errors

--- intake-notes.sh
+++ intake-notes.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-
+#
 #SBATCH --partition=cpu-t2
+#SBATCH --job-name=notes
 #SBATCH --qos=short
-#SBATCH --nodes=8
+#SBATCH --nodes=4
 #SBATCH --tasks-per-node=2
 #SBATCH --cpus-per-task=1 		# Number of cores per task
-#SBATCH --job-name=Notes
-#SBATCH --error=Notes.%J.stderr
-#SBATCH --output=Notes.%J.stdout
+#SBATCH --output=notes_%J.log
+#SBATCH --output=slurm.out
+#SBATCH --error=slurm.err
 
 printf 'Loading modules\n'
 module load R/4.1.2-foss-2021b Anaconda3/2022.05
 
 printf 'Using R\n'
-R CMD BATCH intake-notes.R notes.out
+Rscript ./intake-notes.R 
 
 printf 'Completed notes task\n'
