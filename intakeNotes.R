a130d90f18065c7ef827811e788de8dacb36f37c
asshah4 <mrshahman@gmail.com>
2023-06-26 14:15:11 -0500

    reorganize

--- intakeNotes.R
+++ intakeNotes.R
@@ -7,14 +7,17 @@ library(doMC)
 library(foreach)
 readr::write_lines("Loaded libraries\n", file = "notes.txt")
 
-# Get SLURM variable
+# Get number of computer cores available (essentially CPUs)
 numCPU <-
 	Sys.getenv("SLURM_CPUS_PER_TASK") |>
 	as.numeric()
+if (is.na(numCPU)) {
+	numCPU <- parallel::detectCores()
+}
 readr::write_lines(paste(numCPU, "CPUs available\n"), file = "notes.txt", append = TRUE)
 
 # From PWD, data should be in data folder
-fileNames <- fs::dir_ls("data/ccts/raw", glob = "*.csv")
+fileNames <- fs::dir_ls("tests/ccts/raw", glob = "*.csv")
 numFiles <- length(fileNames)
 readr::write_lines(paste(numFiles, "files to analyze\n"), file = "notes.txt", append = TRUE)
 
