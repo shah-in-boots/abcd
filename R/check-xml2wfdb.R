12e007bf7217e59a9f34b7164f94b0e0ebc9d74f
Anish Shah <asshah4@users.noreply.github.com>
2023-07-14 20:02:51 +0000

    WFDB needs update

--- R/check-xml2wfdb.R
+++ R/check-xml2wfdb.R
@@ -25,20 +25,24 @@ museFolders <- fs::dir_ls(museDir, type = "directory")
 museLog <- fs::path(museDir, "contents.tsv")
 
 # Read in if pre-existing data exists
+cat("Reading in the MUSE directory of files located at...\n", museLog, "\n\n")
 log <- readr::read_tsv(museLog)
 
 # Check / make a list of all MUSE files
 museList <- list()
 
+cat("Will now loop through", length(museFolders), "located at...\n", museDir, "\n\n")
 for (i in seq_along(museFolders)) {
 
 	folderPath <- museFolders[i]
 	folderName <- fs::path_file(folderPath)
 
+	cat("Retrieving names of all XML files in", folderName, "\n")
 	xmlFiles <-
-		fs::dir_ls(folderName, glob = "*.xml") |>
+		fs::dir_ls(folderPath, glob = "*.xml") |>
 		fs::path_file() |>
 		fs::path_ext_remove()
+	cat("There are", length(xmlFiles), "in", folderName, "\n\n")
 
 	museList[[i]] <-
 		tibble::tibble(
@@ -55,12 +59,13 @@ museTable <- dplyr::bind_rows(museList)
 
 # Number of duplicate or updating values
 n <- nrow(dplyr::setdiff(museTable, log))
-cat("When updating the MUSE directories, there are", n, "additions")
 
 updatedLog <-
 	dplyr::bind_rows(museTable, log) |>
 	dplyr::distinct(NAME, .keep_all = TRUE)
 
+cat("When updating the MUSE directory, there were", n, "additions\n")
+cat("These will be written back to...\n", museLog, "\n\n")
 readr::write_tsv(updatedLog, file = museLog)
 
 # WFDB ----
@@ -71,18 +76,22 @@ wfdbLog <- fs::path(wfdbDir, "contents.tsv")
 
 # Read-in pre-existing contents that were "already ran"
 # Will then add/update with individual logs created within directories
+cat("Reading in the WFDB directory of files located at...\n", wfdbLog, "\n\n")
 log <- readr::read_tsv(wfdbLog)
 
 # Needs to be updated for what XML files were converted
 wfdbList <- list()
+cat("Will now loop through", length(wfdbFolders), "located at...\n", wfdbDir, "\n\n")
 for (i in seq_along(wfdbFolders)) {
 
 	folderPath <- wfdbFolders[i]
 	folderName <- fs::path_file(folderPath)
 
+	cat("Retrieving the log of WFDB files in", folderName, "\n")
 	logData <-
 		fs::path(folderPath, "log.txt") |>
 		readr::read_lines()
+	cat("There are", length(logData), "WFDB-formated files in", folderName, "\n\n")
 
 	wfdbList[[i]] <-
 		tibble::tibble(
@@ -95,7 +104,6 @@ wfdbTable <- dplyr::bind_rows(wfdbList)
 
 # Number of duplicate or updating values
 n <- nrow(dplyr::setdiff(wfdbTable, log))
-cat("When updating the WFDB file conversion list, there are", n, "new additions")
 
 # Update WFDB log here
 # These are files that have already been processed
@@ -105,4 +113,6 @@ updatedLog <-
 	dplyr::bind_rows(wfdbTable, log) |>
 	dplyr::distinct(NAME, .keep_all = TRUE)
 
+cat("When updating the WFDB file conversion list, there were", n, "new additions\n")
+cat("These will be written back to...\n", wfdbLog, "\n\n")
 readr::write_tsv(updatedLog, file = wfdbLog)
--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
 #SBATCH --partition=cpu-t3
-#SBATCH --job-name=setupWFDB
+#SBATCH --job-name=checkWFDB
 #SBATCH --nodes=1
 #SBATCH --tasks-per-node=1
 #SBATCH --cpus-per-task=2 		# Number of cores per task
@@ -14,6 +14,6 @@ printf 'Loading modules\n'
 module load R/4.2.1-foss-2022a
 
 printf 'R script to be run'
-Rscript R/setup-xml2wfdb.R
+#Rscript R/setup-xml2wfdb.R
 Rscript R/check-xml2wfdb.R
 
