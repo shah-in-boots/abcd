c0d40e35ad7f46ec860558bd4e23750ce2890bf5
Anish Shah <asshah4@users.noreply.github.com>
2023-07-23 21:37:21 +0000

    meds

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
 #SBATCH --partition=cpu-t3
-#SBATCH --job-name=checkWFDB
+#SBATCH --job-name=meds
 #SBATCH --nodes=1
 #SBATCH --tasks-per-node=1
 #SBATCH --cpus-per-task=2 		# Number of cores per task
@@ -14,5 +14,5 @@ printf 'Loading modules\n'
 module load R/4.2.1-foss-2022a
 
 printf 'R script to be run:\n\'
-Rscript R/filter-demographics2mrn.R
+Rscript R/filter-meds2id.R
 
