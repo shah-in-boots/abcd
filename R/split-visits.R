4eb00158ce4f53937499031aa3a65c305e879e91
Anish Shah <asshah4@users.noreply.github.com>
2023-07-25 12:24:53 -0500

    split wfdb files

--- R/split-visits.R
+++ R/split-visits.R
@@ -1,43 +1,54 @@
 #!/usr/bin/env Rscript
 
-# Take each file and split by years
-args <- commandArgs(trailingOnly = TRUE)
-year <- as.numeric(args[1])
+# Each folder name will be a batch script
+# Each run of the script will parse through the folder to find files
+# If a .dat and .hea file are in a specified year, will move to that folder
+args <- commandArgs(trailingOnly = TRU)E
+folderName <- as.character(args[1])
 
 # year <- 2010
 
 # Libraries
 library(tidyverse)
-library(data.table)
 library(vroom)
 
 # Input paths
 home <- fs::path_expand("~")
 main <- fs::path("projects", "cbcd")
-fileName <- "visits"
+dataFolder <- fs::path(home, main, "data", "wfdb", folderName)
 
-dataFile <- fs::path(home, main, "data", "ccts", "raw", fileName, ext = "csv")
-
-dat <-
-	dataFile |>
-	vroom::vroom(
-		col_select = c(
-			record_id = "RECORD_ID",
-			encounter_id = "ENCOUNTER_ID",
-			visit_location = "ENCOUNTER_TYPE",
-			visit_type = "VISIT_TYPE",
-			visit_discharge = "DISCHARGE_DISPOSITION",
-			start_date = "VISIT_START_DATE",
-			end_date = "VISIT_END_DATE"
-		)
-	) |>
-	dplyr::mutate(date = as.Date(start_date)) |>
-	dplyr::filter(year == lubridate::year(date))
-
-# Output paths
-outputFolder <- fs::path(home, main, "data", "ccts", year)
-if (!fs::dir_exists(outputFolder)) {
-	fs::dir_create(outputFolder)
+# Output folders
+years <- seq(2010, 2023)
+for (i in years) {
+	outputFolder <- fs::path(home, main, "data", "wfdb", i)
+	if (!fs::dir_exists(outputFolder)) {
+		fs::dir_create(outputFolder)
+	}
 }
-outputFile <- fs::path(outputFolder, fileName, ext = "tsv")
-vroom::vroom_write(x = dat, file = outputFile)
+
+
+# Files that need to check for years on
+headerFiles <- fs::dir_ls(dataFolder, glob = "*.hea")
+
+for (i in seq_along(headerFiles)) {
+
+	fileName <- fs::path_file(headerFiles[i]) |> fs::path_ext_remove()
+
+	# Define based on header information of the year of study
+	header <- readr::read_lines(headerFiles[i])
+	dt <- as.Date(str_sub(header[1], start = -10), format = "%d/%m/%Y")
+	year <- lubridate::year(dt)
+	yearFolder <- fs::path(home, main, "data", "wfdb", year)
+
+	# Create folder if needed
+	if (!fs::dir_exists(yearFolder)) {
+		fs::dir_create(yearFolder)
+	}
+
+	# Move file based on name
+	headerPath <- headerFiles[i]
+	signalPath <- fs::path(dataFolder, fileName, ext = "dat")
+	fs::file_move(c(headerPath, signalPath), yearFolder)
+
+}
+
---
+++ R/split-wfdb.R
@@ -0,0 +1,44 @@
+#!/usr/bin/env Rscript
+
+# Take each file and split by years
+args <- commandArgs(trailingOnly = TRUE)
+year <- as.numeric(args[1])
+
+# year <- 2010
+
+# Libraries
+library(tidyverse)
+library(data.table)
+library(vroom)
+
+# Input paths
+home <- fs::path_expand("~")
+main <- fs::path("projects", "cbcd")
+fileName <- "notes"
+
+dataFile <- fs::path(home, main, "data", "ccts", "raw", fileName, ext = "csv")
+
+dat <-
+	dataFile |>
+	vroom::vroom(
+		col_types = "ncnnTTfffc",
+		col_select = c(
+			record_id = "RECORD_ID",
+			encounter_id = "ENCOUNTER_ID",
+			author_type = "AUTHOR_TYPE",
+			author_service = "SERVICE",
+			note_date = "NOTE_DATE",
+			note_type = "NOTE_TYPE",
+			note_text = "NOTE_TEXT"
+		)
+	) |>
+	dplyr::mutate(date = as.Date(note_date)) |>
+	dplyr::filter(year == lubridate::year(date))
+
+# Output paths
+outputFolder <- fs::path(home, main, "data", "ccts", year)
+if (!fs::dir_exists(outputFolder)) {
+	fs::dir_create(outputFolder)
+}
+outputFile <- fs::path(outputFolder, fileName, ext = "tsv")
+vroom::vroom_write(x = dat, file = outputFile)
