2e3dd3d43f5b36e435e5f18ef8736b1754d0718e
Anish Shah <asshah4@users.noreply.github.com>
2023-07-24 22:12:03 -0500

    working on reorganizing the data

---
+++ R/split-diagnosis.R
@@ -0,0 +1,39 @@
+# Take each file and split by years
+args <- commandArgs(trailingOnly = TRUE)
+year <- as.numeric(args[1])
+
+# year <- 2010
+
+# Libraries
+library(tidyverse)
+library(data.table)
+library(vroom)
+
+# Input paths
+home <- fs::path_expand("~")
+main <- fs::path("projects", "cbcd", "sandbox")
+fileName <- "diagnosis"
+
+dataFile <- fs::path(home, main, "data", "ccts", "raw", fileName, ext = "csv")
+
+dat <-
+	dataFile |>
+	vroom::vroom(
+		col_select = c(
+			record_id = "RECORD_ID",
+			encounter_id = "ENCOUNTER_ID",
+			date = "START_DATE",
+			icd_code = "ICD10_CODE"
+		),
+		col_types = "nnDc"
+	) |>
+	dplyr::mutate(date = as.Date(date)) |>
+	dplyr::filter(year == lubridate::year(date))
+
+# Output paths
+outputFolder <- fs::path(home, main, "data", "ccts", year)
+if (!fs::dir_exists(outputFolder)) {
+	fs::dir_create(outputFolder)
+}
+outputFile <- fs::path(outputFolder, fileName, ext = "tsv")
+vroom::vroom_write(x = dat, file = outputFile)
