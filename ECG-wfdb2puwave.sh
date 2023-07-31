f64980b35341afc48349ce2e0aa68dd7eb5b6eb5
Anish Shah <asshah4@users.noreply.github.com>
2023-07-31 19:08:57 +0000

    need to revise

---
+++ ECG-wfdb2puwave.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+#SBATCH --partition=cpu-t3
+#SBATCH --job-name=ecgpuwave
+#SBATCH --nodes=14
+#SBATCH --tasks-per-node=1
+#SBATCH --cpus-per-task=2		# Number of cores per task
+#SBATCH --array=1-14
+#SBATCH --error=slurm-%A-%a.err
+#SBATCH --output=slurm-%A-%a.out
+#SBATCH --mail-user=ashah282@uic.edu
+#SBATCH --mail-type=END
+
+printf 'Load modules\n'
+module load R/4.2.1-foss-2022a
+
+# Years setup (2010 to 2023 is 14...)
+years=($(seq 2010 2023))
+year=${years[$SLURM_ARRAY_TASK_ID - 1]}
+
+# Past to R script with variable for years
+# Remember to update partition if using large files (e.g. notes)
+printf "Create ECGPUWAVE annotations for the following time: $year"
+Rscript R/annotate-wfdb2puwave.R $year
