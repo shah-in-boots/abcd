889adc0a8c016b7c7339b97e684d1ee89f435eab
Anish Shah <asshah4@users.noreply.github.com>
2023-08-14 03:18:41 +0000

    afib filter worked

--- run-job.sh
+++ run-job.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-#SBATCH --partition=cpu-c5
-#SBATCH --job-name=findWFDBdx
+#SBATCH --partition=cpu-t3
+#SBATCH --job-name=duplicates
 #SBATCH --nodes=1
 #SBATCH --tasks-per-node=1
 #SBATCH --error=slurm-%J.err
 #SBATCH --output=slurm-%J.out
 
 module load R/4.2.1-foss-2022a
-Rscript R/copy-mrn2wfdb.R sandbox/wes.csv wes 20 30
+Rscript R/tidy-duplicateCSV.R data/ccts/sdoh
 
