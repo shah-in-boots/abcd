26d8a0a65987e046dc2f8f36eccf8127e13460bd
Anish Shah <asshah4@users.noreply.github.com>
2023-10-22 19:52:32 -0500

    update sandbox

--- sandbox.R
+++ sandbox.R
@@ -16,6 +16,7 @@ demo <- read_csv("./sandbox/data/ccts/raw/redcap-ids.csv")
 
 # CCTS data for TTE ----
 
+library(gtsummary)
 ids <- vroom::vroom_lines('output/mrn-hf.txt')
 dat <-
 	vroom::vroom('data/ccts/raw/redcap-ids.csv',
@@ -24,6 +25,29 @@ dat <-
 	select(age, gender, race, insurance_type)
 
 dat |>
+	mutate(race = case_when(
+		race == 1 ~ 'American Indian or Alaskan',
+		race == 2 ~ 'Asian',
+		race == 3 ~ 'Native Hawaiian or Pacific Islander',
+		race == 4 ~ 'Black or African American',
+		race == 5 ~ 'White',
+		race == 6 ~ 'Other race',
+		race == 7 ~ 'Patient declined',
+		race == 8 ~ 'No information'
+	)) |>
+	mutate(insurance_type = case_when(
+		insurance_type == 1 ~ 'Private insurance',
+		insurance_type == 2 ~ 'Medicaid',
+		insurance_type == 3 ~ 'Medicare',
+		insurance_type == 4 ~ 'Self-pay',
+		insurance_type == 5 ~ 'Other',
+		insurance_type == 6 ~ 'No information'
+	)) |>
+	mutate(gender = case_when(
+		gender == 0 ~ 'Unknown',
+		gender == 1 ~ 'Male',
+		gender == 2 ~ 'Female'
+	)) |>
 	tbl_summary(by = race)
 
 
