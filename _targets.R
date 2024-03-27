9d30f568a0da07b5506905975e749bbc8dfa5826
Anish S. Shah <asshah4@users.noreply.github.com>
2024-03-27 11:35:53 -0500

    modify targets

--- _targets.R
+++ _targets.R
@@ -1,50 +1,58 @@
 library(targets)
 library(tarchetypes)
+library(crew)
 
 # Options
 tar_option_set(
   packages = c(
-    # Systems and cluster computing
-    'reticulate',
-    # Personal libraries,
-    'shiva', 'volundr',
-    # Specific data type handling
-    'data.table', 'xml2',
-    # Content specific (e.g. clinical)
-    'icd',
-    # Wrangling
-    'tibble', 'tidyverse', 'here', 'fs', 'vroom',
-    # Modeling,
-    'tidymodels',
-    # Presenting,
-    'gt', 'gtsummary'
-    ),
-  format = 'qs' # default storage format
-  # Set other options as needed.
+    # Infrastructure
+    "targets", "tarchetypes", "crew", "crew.cluster",
+    "here", "fs",
+    "arrow", "qs", "fst",
+    # Data intake and cleaning
+    "REDCapR",
+    "tidyverse", "lubridate", "clock",
+    "janitor", "tidytext",
+    "data.table", "xlsx", "readxl",
+    # Epidemiology
+    "survival",
+    # Clinical
+    "comorbidity",
+    # Machine learning
+    "tidymodels",
+    "reticulate", "tensorflow", "tfdatasets", "keras",
+    # Presenting
+    "knitr", "gt", "gtsummary", "glue",
+    "ggdag", "ggsurvfit", "ggsci",
+    # Validating
+    "labelled", "pointblank", "naniar",
+    # Personal
+    "vlndr", "shiva", "card"
+  )
 )
 
 # tar_make_clustermq() configuration (okay to leave alone):
-options(clustermq.scheduler = 'multicore')
+options(clustermq.scheduler = "multicore")
 
 # tar_make_future() configuration (okay to leave alone):
 future::plan(future.callr::callr)
 
 # Scripts
-source('R/target-options.R')
-source('R/target-intake.R')
-source('R/target-tidy.R')
+source("R/target-options.R")
+source("R/target-intake.R")
+source("R/target-tidy.R")
 
 # Targets
 list(
 
   # General ----
-  tar_target(data_loc, fs::path(here::here(), 'data')),
-  tar_target(afib_data_loc, fs::path(data_loc, 'ccts', 'afib')),
+  tar_target(data_loc, fs::path(here::here(), "data")),
+  tar_target(afib_data_loc, fs::path(data_loc, "ccts", "afib")),
 
   tar_target(
     cardiac_medications, # Simplified CV meds
     read_in_cardiac_medications(dataFolder = afib_data_loc,
-                           regexFile = 'regex-meds.txt')
+                           regexFile = "regex-meds.txt")
   ),
 
   # AFIB ----
@@ -58,7 +66,7 @@ list(
     read_in_afib_diagnoses(dataFolder = afib_data_loc)
   ),
 
-  tar_quarto(sdoh_slides, 'output/slides-sdoh.qmd')
+  tar_quarto(sdoh_slides, "output/slides-sdoh.qmd")
 
   # WES ----
 )
