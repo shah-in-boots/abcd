18eb3da6c0e0e1719aadfde120531ad76fc2564d
Anish S. Shah <asshah4@users.noreply.github.com>
2024-03-27 13:32:40 -0500

    toy example

--- _targets.R
+++ _targets.R
@@ -7,9 +7,14 @@ library(crew.cluster)
 controller <- crew_controller_slurm(
   workers = 2L,
   host = "ashah282@aws.acer.uic.edu",
-  tasks_max = 2,
   seconds_idle = 30,
-  script_lines = "module load R/4.2.1-foss-2022a"
+  script_directory = "~/projects/cbcd/jobs",
+  script_lines = "module load R/4.2.1-foss-2022a",
+  verbose = TRUE,
+  slurm_log_output = "crew_log_%A.txt",
+  slurm_log_error = "crew_log_%A.txt",
+  slurm_cpus_per_task = 2,
+  slurm_partition = "cpu-t3"
 )
 
 # Options
@@ -45,6 +50,11 @@ tar_option_set(
 list(
   tar_target(cbcd_data_loc, "~/data/cbcd"),
   tar_target(aflubber_data_loc, "~/data/aflubber"),
-  tar_target(genetics_data_loc, "~/data/genetics")
+  tar_target(genetics_data_loc, "~/data/genetics"),
 
+  tar_target(datasetA, sum(1:10)),
+  tar_target(outputA, writeLines(dataset, con = "output-a.txt")),
+
+  tar_target(datasetB, sum(11:20)),
+  tar_target(outputB, writeLines(dataset, con = "output-b.txt")),
 )
