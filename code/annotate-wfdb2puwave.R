39aa279ede45800854835c203fc9ebeebdeb50db
dsean2112 <152893658+dsean2112@users.noreply.github.com>
2025-03-27 22:22:24 -0500

    Update annotate-wfdb2puwave.R
    adapted for new cluster

--- code/annotate-wfdb2puwave.R
+++ code/annotate-wfdb2puwave.R
@@ -17,13 +17,14 @@ library(vroom)
 library(foreach)
 library(parallelly)
 library(doParallel)
-options(wfdb_path = '/mmfs1/home/dseaney2/wfdb/bin') # can also add 'source $HOME/.bashrc' to .sh file before R script 
+# options(wfdb_path = '/mmfs1/home/dseaney2/wfdb/bin') # can also add 'source $HOME/.bashrc' to .sh file before R script 
+source("/mmfs1/projects/cardio_darbar_chi/common/data/custom_install_code/wfdb-annotation-temp.R") # temp corrected code
 
 # Arguments
 # 	1st = SLURM_ARRAY_JOB_ID
 #		2nd = SLURM_ARRAY_TASK_COUNT
 args <- commandArgs(trailingOnly = TRUE)
-taskNumber <- as.character(args[1]) # Example... 3rd job
+taskNumber <- as.integer(args[1]) # Example... 3rd job
 taskCount <- as.integer(args[2]) # Total array jobs will be the number of nodes
 cat("\tBatch array job number", taskNumber, "out of", taskCount, "array jobs total\n")
 
@@ -33,18 +34,19 @@ doParallel::registerDoParallel(cores = nCPU)
 cat("\tAttempting parallelization with", nCPU, "cores\n")
 
 # Paths
-home <- fs::path('/mmfs1/projects/cardio_darbar_chi/common/data')
-# main <- fs::path("data")
-wfdb <- fs::path(home, "wfdb")
+home <- fs::path('/mmfs1/projects/cardio_darbar_chi/common/')
+wfdb <- fs::path(home, "data", "wfdb")
+muse <- fs::path(home, "data", "muse")
 
 # Batch Preparation ----
 
 # Number of files to be split into ~ equivalent parts
 inputData <- read.csv(fs::path(wfdb, 'wfdb', ext = 'log'))
+# Need to swap old file name contained with in $PATH to the new file name
+inputData$PATH <- fs::path(dirname(inputData$PATH),inputData$FILE_NAME)
 
 # Create splits for batching
-splitData <-
-	split(inputData, cut(seq_along(inputData), taskCount, labels = FALSE))
+splitData <- split(inputData, cut(1:nrow(inputData), taskCount, labels = FALSE))
 chunkData <- splitData[[taskNumber]]
 cat("\tWill consider", nrow(chunkData), "WFDB files in this batch\n")
 
@@ -58,60 +60,75 @@ cat("\nPreparing WFDB data for annotation:\n\n")
 
 # Log file information
 logFile <- fs::path(wfdb, 'ecgpuwave', ext = 'log')
-if (!fs::file_exists(logFile)) {
+if (!fs::file_exists(logFile)) { # create new file if needed
 	fs::file_create(logFile)
-}
-logData <- vroom::vroom_lines(logFile)
-cat("\tCurrently there are", length(logData), "files in the ECGPUWAVE log\n")
+	logData <- data.frame(MUSE_ID = character(),
+			      PATH = character(),
+			      FILE_NAME = character(),
+			      stringsAsFactors = FALSE)  # Avoid converting character columns to factors
+} else {logData <- read.csv(logFile)}
+
+cat("\tCurrently there are", nrow(logData), "files in the ECGPUWAVE log\n")
 
 # Only need to annotate those that have not yet been done
-newData <- setdiff(chunkData, logData)
-cat("\tThere are", length(newData), "WFDB files that can be annotated\n")
+newData <- chunkData |> dplyr::filter(!FILE_NAME %in% logData$FILE_NAME)
+cat("\tThere are", nrow(newData), "WFDB files that can be annotated\n")
 
-
-# Annotation ----
-
-fs::file_delete(fs::dir_ls(wfdb, recurse = TRUE, glob = "*.ecgpuwave"))
-
-# Need a list of all paths to evaluate
-wfdbPaths <-
-	fs::dir_ls(wfdb, recurse = TRUE, type = "file", glob = "*.dat")
-
-filePaths <-
-	sapply(newData,
-				 function(.x) {
-				 	fs::path_filter(wfdbPaths, regexp = .x)
-				 },
-				 USE.NAMES = FALSE) |>
-	fs::as_fs_path()
-
-fileNames <- fs::path_file(filePaths) |> fs::path_ext_remove()
-n <- length(fileNames)
+start <- 1
+# end <- nrow(newData)
+end <- 10
 
 # Make sure parallel is set up earlier
 # Also place everything into correct "folder" by YEAR
-annotatedFiles <-
-	foreach(i = 1:n, .combine = 'c', .errorhandling = "remove") %do% {
+out <-
+	foreach(i = start:end, .combine = 'rbind', .errorhandling = "remove") %do% {
 
-		if (n > 0) {
+		if (end > 0) {
 			# Read in individual files and locations
-			fn <- fileNames[i]
-			fp <- filePaths[i]
+			fn <- newData$FILE_NAME[i]
+			fp <- fs::path(home,newData$PATH[i])
 			fd <- fs::path_dir(fp)
 			year <- fs::path_split(fd)[[1]] |> dplyr::last()
 
-			EGM::read_annotation(
+			annotate_wfdb(
 				record = fn,
 				record_dir = fd,
-				detector = "ecgpuwave"
+				annotator = "ecgpuwave"
 			)
 
-			vroom::vroom_write_lines(fn, logFile, append = TRUE)
+
+			# Move file to correct folder
+			old_path <- newData$PATH[i]
+			new_path <- gsub("wfdb", "ecgpuwave", old_path)
+			file.rename(from=fs::path(home,old_path,ext='ecgpuwave'),
+				    to=fs::path(home,new_path,ext='ecgpuwave'))
+			
+			# vroom::vroom_write_lines(fn, logFile, append = TRUE)
 			cat("\tWrote the file", fn, "into the", year, "folder\n")
 
-			# "Return value"
-			fn
+			# "Return new row"
+			
+			if (file.exists(fs::path(home,new_path,ext='ecgpuwave'))) {
+			data.frame(
+				MUSE_ID = newData$MUSE_ID[i],
+				PATH = new_path,
+				FILE_NAME = fn,
+				stringsAsFactors = FALSE
+				)
+				}
 		}
 	}
 
-cat("\tA total of", length(annotatedFiles), "were added to the ECGPUWAVE log\n")
+out |>
+#  as.data.frame() |>
+  dplyr::distinct() |>
+  vroom::vroom_write(
+    file = logFile,
+    delim = ",",
+    col_names = FALSE,
+    append = TRUE
+  )
+
+cat("\tA total of", nrow(out), "were added to the ECGPUWAVE log\n")
+
+
