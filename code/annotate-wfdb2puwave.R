1e177b3cf0cf228e7dad04e136e8c07ffc82d93d
dsean2112 <152893658+dsean2112@users.noreply.github.com>
2025-03-31 21:28:38 -0500

    Update annotate-wfdb2puwave.R
    adapted to move to/from scratch space

--- code/annotate-wfdb2puwave.R
+++ code/annotate-wfdb2puwave.R
@@ -29,6 +29,8 @@ taskNumber <- as.integer(args[1]) # Example... 3rd job
 taskCount <- as.integer(args[2]) # Total array jobs will be the number of nodes
 cat("\tBatch array job number", taskNumber, "out of", taskCount, "array jobs total\n")
 
+jobSize <- 10000 # total number of files (across all tasks). If you would like to read all files, simply comment this line
+
 # Setup parallelization
 nCPU <- parallelly::availableCores()
 doParallel::registerDoParallel(cores = nCPU)
@@ -38,31 +40,15 @@ cat("\tAttempting parallelization with", nCPU, "cores\n")
 home <- fs::path('/mmfs1/projects/cardio_darbar_chi/common/')
 wfdb <- fs::path(home, "data", "wfdb")
 muse <- fs::path(home, "data", "muse")
+ecgpuwave <- fs::path(home, "data", "ecgpuwave")
+scratch <- fs::path(Sys.getenv("SCRATCH_LOCAL")) # added path for scratch space
 
-# Batch Preparation ----
-
-# Number of files to be split into ~ equivalent parts
+# wfdb Log file information
 inputData <- read.csv(fs::path(wfdb, 'wfdb', ext = 'log'))
 # Need to swap old file name contained with in $PATH to the new file name
 inputData$PATH <- fs::path(dirname(inputData$PATH),inputData$FILE_NAME)
 
-# Create splits for batching
-if (taskCount > 1) {
-	splitData <- split(inputData, cut(1:nrow(inputData), taskCount, labels = FALSE))
-	chunkData <- splitData[[taskNumber]]
-	rm(splitData)
-} else {chunkData <- inputData}
-cat("\tWill consider", nrow(chunkData), "WFDB files in this batch\n")
-
-# Clean up potentially large vectors
-rm(inputData)
-gc()
-
-# WFDB Preparation ----
-
-cat("\nPreparing WFDB data for annotation:\n\n")
-
-# Log file information
+# ecgpuwave Log file information
 logFile <- fs::path(wfdb, 'ecgpuwave', ext = 'log')
 if (!fs::file_exists(logFile)) { # create new file if needed
         fs::file_create(logFile)
@@ -74,47 +60,111 @@ if (!fs::file_exists(logFile)) { # create new file if needed
 
 cat("\tCurrently there are", nrow(logData), "files in the ECGPUWAVE log\n")
 
+# Batch Preparation ----
+
 # Only need to annotate those that have not yet been done
-newData <- chunkData |> dplyr::filter(!FILE_NAME %in% logData$FILE_NAME)
-cat("\tThere are", nrow(newData), "WFDB files that can be annotated\n")
+newData <- inputData |> dplyr::filter(!FILE_NAME %in% logData$FILE_NAME)
+rm(inputData)
+
+# Limit file count as needed
+if (exists("jobSize")) {
+	newData <- newData[1:jobSize,]
+}
+
+# Create folders
+years <- unique(sapply(strsplit(newData$PATH, "/"), function(x) x[3]))
+for (year in years) {
+	folder_path <- fs::path(scratch, year)
+	if (!fs::dir_exists(folder_path)) {
+		fs::dir_create(folder_path)
+		cat("Created folder:", folder_path, "\n")
+	} else {
+		cat("Folder already exists:", folder_path, "\n")
+	}
+}
+		       
+# Copy files to scratch space
+cat("Copying files to scratch...","\n")
+for (i in 1:nrow(newData)) {
+	# Extract the file path
+	file_path_hea <- fs::path(home, newData$PATH[i],ext='hea')
+	file_path_dat <- fs::path(home, newData$PATH[i],ext='dat')
+  
+	# Extract the 2nd to last term from the file path (the containing folder)
+	components <- strsplit(newData$PATH[i], "/")[[1]]
+	folder_name <- components[length(components) - 1]
+  
+	# Construct target paths
+	file_name <- newData$FILE_NAME[i]
+	target_path_hea <- fs::path(scratch, folder_name, paste0(file_name, ".hea"))
+	target_path_dat <- fs::path(scratch, folder_name, paste0(file_name, ".dat"))
+	  
+	# Copy files to the target paths
+	fs::file_copy(file_path_hea, target_path_hea, overwrite = TRUE)
+	fs::file_copy(file_path_dat, target_path_dat, overwrite = TRUE)
+  
+ 	# cat("Copied file", file_path, "to", target_path_hea, "and", target_path_dat, "\n")
+	if (!i %% 2000) {
+		cat(i,"of",nrow(newData),"\n")
+		}
+}
+
+# Create splits for batching
+if (taskCount > 1) {
+	splitData <- split(newData, cut(1:nrow(newData), taskCount, labels = FALSE))
+	chunkData <- splitData[[taskNumber]]
+	rm(splitData)
+} else {chunkData <- newData}
+cat("\tWill consider", nrow(chunkData), "WFDB files in this batch\n")
+
+# Clean up potentially large dataframes
+rm(newData)
+gc()
+
+# WFDB Preparation ----
+
+cat("\nPreparing WFDB data for annotation:\n\n")
+
+cat("\tThere are", nrow(chunkData), "WFDB files that can be annotated\n")
 
 start <- 1
-# end <- nrow(newData)
-end <- 11000
+end <- nrow(chunkData)
 
 # Make sure parallel is set up earlier
 # Also place everything into correct "folder" by YEAR
+
+# Write files to scratch space
 out <-
         foreach(i = start:end, .combine = 'rbind', .errorhandling = "remove") %do% {
 
                 if (end > 0) {
                         # Read in individual files and locations
-                        fn <- newData$FILE_NAME[i]
-                        fp <- fs::path(home,newData$PATH[i])
+                        fn <- chunkData$FILE_NAME[i]
+                        fp <- fs::path(home,chunkData$PATH[i])
                         fd <- fs::path_dir(fp)
                         year <- fs::path_split(fd)[[1]] |> dplyr::last()
 
                         annotate_wfdb(
                                 record = fn,
-                                record_dir = fd,
+                                record_dir = fs::path(scratch,year),
                                 annotator = "ecgpuwave"
                         )
 
 
                         # Move file to correct folder
-                        old_path <- newData$PATH[i]
-                        new_path <- gsub("wfdb", "ecgpuwave", old_path)
-                        file.rename(from=fs::path(home,old_path,ext='ecgpuwave'),
-                                    to=fs::path(home,new_path,ext='ecgpuwave'))
+                        # old_path <- chunkData$PATH[i]
+                        # new_path <- gsub("wfdb", "ecgpuwave", old_path)
+                        # file.rename(from=fs::path(home,old_path,ext='ecgpuwave'),
+                        #             to=fs::path(home,new_path,ext='ecgpuwave'))
 
                         # vroom::vroom_write_lines(fn, logFile, append = TRUE)
-                        cat("\tWrote the file", fn, "into the", year, "folder\n")
+                        cat("\tWrote the file", fn, "into the", year, "scratch folder\n")
 
-                        # "Return new row"
+                        # Return new row
 
                         if (file.exists(fs::path(home,new_path,ext='ecgpuwave'))) {
                         data.frame(
-                                MUSE_ID = newData$MUSE_ID[i],
+                                MUSE_ID = chunkData$MUSE_ID[i],
                                 PATH = new_path,
                                 FILE_NAME = fn,
                                 stringsAsFactors = FALSE
@@ -122,6 +172,35 @@ out <-
                                 }
                 }
         }
+
+# Copy files from scratch space to project space ---
+# Get list of subfolders in scratch
+subfolders <- fs::dir_ls(scratch, type = "directory")
+
+# Loop through each subfolder
+for (subfolder in subfolders) {
+  folder_name <- fs::path_file(subfolder)
+  
+  # Get list of .ecgpuwave files in the current subfolder
+  ecgpuwave_files <- fs::dir_ls(subfolder, glob = "*.ecgpuwave")
+  
+  # Define corresponding 'ecgpuwave' folder path
+  ecgpuwave_subfolder <- fs::path(ecgpuwave, folder_name)
+  
+  # Create the 'ecgpuwave' subfolder if it doesn't exist
+  if (!fs::dir_exists(ecgpuwave_subfolder)) {
+    fs::dir_create(ecgpuwave_subfolder)
+    cat("Created folder:", ecgpuwave_subfolder, "\n")
+  }
+  
+  # Move each .ecgpuwave file to the corresponding 'ecgpuwave' subfolder
+  for (file in ecgpuwave_files) {
+    target_path <- fs::path(ecgpuwave_subfolder, fs::path_file(file))
+    fs::file_copy(file, target_path, overwrite = TRUE)
+    # cat("Moved file:", file, "to", target_path, "\n")
+  }
+}
+
 
 out |>
 #  as.data.frame() |>
